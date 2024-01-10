275cf5628e8ac622965490ff0eeada51e0cc9200
Simen Didrichsen <108331982+didrichsen@users.noreply.github.com>
2024-01-10 16:04:11 +0100

    git init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <>
+      <div>Clean Slate!</div>
+          <div>Change to active branch or create new branch for lectures and exercisers</div>
+      </>
+  )
+}
+
+export default App
