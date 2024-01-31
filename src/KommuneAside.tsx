5dc13aa96bd5b470013f88bb9cb1dba14535eb72
Simen Didrichsen <108331982+didrichsen@users.noreply.github.com>
2024-01-31 14:24:35 +0100

    displaying fylker in aside

--- src/KommuneAside.tsx
+++ src/KommuneAside.tsx
@@ -1,20 +1,26 @@
 import React, {useEffect} from "react";
 import {KommuneContext} from "./context/KommuneContext";
+import VectorSource from "ol/source/Vector";
+import {Feature} from "ol";
 
 const KommuneAside = () => {
 
     const {layer} = React.useContext(KommuneContext);
 
     const kommuneLayer = layer.find((layer) => layer.getClassName() === "kommune");
-
-    useEffect(() => {
-        console.log(kommuneLayer);
-    }, [kommuneLayer]);
+    const features = (kommuneLayer?.getSource()  as VectorSource)?.getFeatures();
 
     return (
 
         <aside className={kommuneLayer? "visible" : "hidden"}>
-            <h2>Aside</h2>
+            <div>
+                <h1>Kommuner</h1>
+            <ul>
+                {features?.map((feature) =>
+                    <li>{feature.getProperties().navn[0].navn}</li>
+                )}
+            </ul>
+            </div>
         </aside>
 
     )
