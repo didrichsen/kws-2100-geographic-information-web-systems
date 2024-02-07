4986f6e925f1d1dfaae5d791455d3b8273007ed0
Simen Didrichsen <108331982+didrichsen@users.noreply.github.com>
2024-02-07 15:54:39 +0100

    fixed issues with hover

--- src/kommune/KommuneAside.tsx
+++ src/kommune/KommuneAside.tsx
@@ -1,19 +1,20 @@
-import React, {createContext, useContext, useEffect, useMemo, useState} from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import { getStedsNavn } from "../utility/utilities";
 import { getFeatures } from "../utility/getFeatures";
 import { GetViewExtend } from "../utility/getViewExtend";
-import {MapContext} from "../context/MapContext";
-import {Feature, MapBrowserEvent} from "ol";
-import {Pixel} from "ol/pixel";
-import {FeatureLike} from "ol/Feature";
-import {Fill, Style} from "ol/style";
-import {Polygon} from "ol/geom";
+import { MapContext } from "../context/MapContext";
+import { Feature, MapBrowserEvent } from "ol";
+import { Pixel } from "ol/pixel";
+import { FeatureLike } from "ol/Feature";
+import { Fill, Style } from "ol/style";
+import { Polygon } from "ol/geom";
 
 const KommuneAside = () => {
+  const [activeFeature, setActiveFeature] =
+    useState<Feature<Polygon>>(undefined);
+
   const features = getFeatures("kommune");
-
-  const {map} = useContext(MapContext);
-
+  const { map } = useContext(MapContext);
   const viewExtend = GetViewExtend();
 
   const visibleFeatures = useMemo(
@@ -24,75 +25,39 @@ const KommuneAside = () => {
     [features, viewExtend],
   );
 
-  const [activeFeature, setActiveFeature] = useState<Feature<Polygon>>(undefined);
+  const handleFeaturesAtPixel = (pixel: Pixel) => {
+    const featuresToStyle: FeatureLike[] = [];
 
-  let styledFeature: FeatureLike | undefined = undefined;
+    map.forEachFeatureAtPixel(pixel, (feature) => {
+      featuresToStyle.push(feature);
+    });
 
-
-  const handleFeaturesAtPixel = (pixel : Pixel) => {
-
-      const featuresToStyle : FeatureLike[] = [];
-/*
-      if(styledFeature){
-          styledFeature.setStyle(null);
-          styledFeature = undefined;
-      }
-
- */
-
-      map.forEachFeatureAtPixel(pixel,(feature) => {
-          featuresToStyle.push(feature)
-      });
-
-      if(featuresToStyle.length === 1){
-      // console.log(featuresToStyle[0]);
+    if (featuresToStyle.length === 1) {
       setActiveFeature(featuresToStyle[0]);
-      //console.log("----");
-      //console.log(activeFeature);
-          //styledFeature = featuresToStyle[0];
-/*
-          const style = new Style({
-              fill: new Fill({
-                  color: 'rgba(200, 200, 200, 0.5)'
-              })
-          })
+    } else {
+      setActiveFeature(undefined);
+    }
+  };
 
- */
+  const style = new Style({
+    fill: new Fill({
+      color: "rgba(200, 200, 200, 0.5)",
+    }),
+  });
 
+  useEffect(() => {
+    activeFeature?.setStyle(style);
+    return () => activeFeature?.setStyle(undefined);
+  }, [activeFeature]);
 
+  useEffect(() => {
+    map.on("pointermove", (e: MapBrowserEvent<MouseEvent>) => {
+      const pixel = e.pixel;
+      handleFeaturesAtPixel(pixel);
+    });
 
-          //activeFeature.setStyle(style);
-          //styledFeature.setStyle(style);
-      } else {
-          setActiveFeature(undefined);
-      }
-  }
-
-    const style = new Style({
-        fill: new Fill({
-            color: 'rgba(200, 200, 200, 0.5)'
-        })
-    })
-
-
-
-
-    useEffect(() => {
-        activeFeature?.setStyle(style);
-        return () => activeFeature?.setStyle(undefined);
-    }, [activeFeature]);
-
-
-
-    useEffect(() => {
-        map.on('pointermove', (e:MapBrowserEvent<MouseEvent>) => {
-            const pixel = e.pixel;
-            handleFeaturesAtPixel(pixel);
-        })
-
-        return () => map.un('pointermove');
-
-    }, []);
+    return () => map.un("pointermove");
+  }, []);
 
   return (
     <aside className={visibleFeatures?.length ? "visible" : "hidden"}>
