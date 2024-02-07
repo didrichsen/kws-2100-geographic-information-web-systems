ea29cc0eacfe54ac51bcc4e19e9b3ac514955a0d
Simen Didrichsen <108331982+didrichsen@users.noreply.github.com>
2024-02-07 11:07:14 +0100

    added support for hovering

--- src/kommune/KommuneAside.tsx
+++ src/kommune/KommuneAside.tsx
@@ -1,10 +1,19 @@
-import React, { useEffect, useMemo } from "react";
+import React, {createContext, useContext, useEffect, useMemo} from "react";
 import { getStedsNavn } from "../utility/utilities";
 import { getFeatures } from "../utility/getFeatures";
 import { GetViewExtend } from "../utility/getViewExtend";
+import {MapContext} from "../context/MapContext";
+import {Feature, MapBrowserEvent} from "ol";
+import {Pixel} from "ol/pixel";
+import {FeatureLike} from "ol/Feature";
+import {Fill, Style} from "ol/style";
 
 const KommuneAside = () => {
   const features = getFeatures("kommune");
+
+  const featuresToStyle : FeatureLike[] = [];
+
+  const {map} = useContext(MapContext);
 
   const viewExtend = GetViewExtend();
 
@@ -15,6 +24,38 @@ const KommuneAside = () => {
       ),
     [features, viewExtend],
   );
+
+  let styledFeature: FeatureLike | undefined = undefined;
+  const handleFeaturesAtPixel = (pixel : Pixel) => {
+
+      if(styledFeature){
+          styledFeature.setStyle(null);
+          styledFeature = undefined;
+      }
+
+      map.forEachFeatureAtPixel(pixel,(feature) => {
+          featuresToStyle.push(feature)
+      });
+
+      if(featuresToStyle.length > 0){
+          styledFeature = featuresToStyle.pop();
+          const style = new Style({
+                  fill: new Fill({
+                      color: 'rgba(200, 200, 200, 0.5)'
+                  })
+          })
+
+          styledFeature?.setStyle(style);
+      }
+  }
+
+    useEffect(() => {
+        map.on('pointermove', (e:MapBrowserEvent<MouseEvent>) => {
+            const pixel = e.pixel;
+            handleFeaturesAtPixel(pixel);
+        })
+
+    }, []);
 
   return (
     <aside className={visibleFeatures?.length ? "visible" : "hidden"}>
