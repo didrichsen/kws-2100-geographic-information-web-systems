bb65664021faa4c9a42790f23d063fdc3c6fcb8f
Simen Didrichsen <108331982+didrichsen@users.noreply.github.com>
2024-02-08 10:34:45 +0100

    created hook for adding layer

--- src/fylke/FylkeLayerCheckbox.tsx
+++ src/fylke/FylkeLayerCheckbox.tsx
@@ -1,36 +1,23 @@
-import React, { useContext, useEffect, useMemo } from "react";
-import { useState } from "react";
-import { MapContext } from "../context/MapContext";
+import React, {useState} from "react";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
-import { GeoJSON } from "ol/format";
+import {GeoJSON} from "ol/format";
+import {useLayer} from "../hooks/UseLayer";
+
+const fylkeLayer = new VectorLayer({
+    className: "fylker",
+    source: new VectorSource({
+        url: "/kws-2100-geographic-information-web-systems/fylker_komprimert.json",
+        format: new GeoJSON(),
+    }),
+});
 
 const FylkeLayerCheckbox = () => {
   const [checked, setChecked] = useState(false);
 
-  const { setLayers } = useContext(MapContext);
+    useLayer(checked, fylkeLayer);
 
-  const fylkeLayer = useMemo(
-    () =>
-      new VectorLayer({
-        className: "fylker",
-        source: new VectorSource({
-          url: "/kws-2100-geographic-information-web-systems/fylker_komprimert.json",
-          format: new GeoJSON(),
-        }),
-      }),
-    [],
-  );
-
-  useEffect(() => {
-    if (checked) {
-      setLayers((old) => [...old, fylkeLayer]);
-    }
-
-    return () => setLayers((old) => old.filter((l) => l != fylkeLayer));
-  }, [checked]);
-
-  return (
+    return (
     <label>
       {checked ? "Remove Fylker layer" : "Add Fylker layer"}
       <input
---
+++ src/hooks/UseLayer.tsx
@@ -0,0 +1,17 @@
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import {useContext, useEffect} from "react";
+import {MapContext} from "../context/MapContext";
+
+export function useLayer(checked: boolean, layer: VectorLayer<VectorSource>) {
+
+    const {setLayers} = useContext(MapContext);
+
+    useEffect(() => {
+        if (checked) {
+            setLayers((old) => [...old, layer]);
+        }
+
+        return () => setLayers((old) => old.filter((l) => l != layer));
+    }, [checked]);
+}
