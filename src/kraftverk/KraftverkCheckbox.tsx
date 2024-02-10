6e93b503a34ea2b3600d9ccd58ed1524047ab322
Simen Didrichsen <108331982+didrichsen@users.noreply.github.com>
2024-02-10 16:56:18 +0100

    push cluster

--- src/kraftverk/KraftverkCheckbox.tsx
+++ src/kraftverk/KraftverkCheckbox.tsx
@@ -5,10 +5,11 @@ import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
 import { useLayer } from "../hooks/UseLayer";
 import { Fill, Stroke, Style } from "ol/style";
-import { Circle } from "ol/style";
+import { Circle as CircleStyle, RegularShape } from "ol/style"; // Changed Circle import to CircleStyle
 import {Feature} from "ol";
 import {FeatureLike} from "ol/Feature";
 import {Point} from "ol/geom";
+import {Cluster} from "ol/source";
 
 type KraftverkFeature = {getProperties():kraftverkProps} & Feature<Point>
 
@@ -16,21 +17,33 @@ interface kraftverkProps {
   maksytelse:number;
 }
 
+const source = new VectorSource({
+  url: "/kws-2100-geographic-information-web-systems/kraftverk.json",
+  format: new GeoJSON(),
+});
+
+const clusterSource = new Cluster({
+  distance: 40,
+  source: source,
+});
+
 const kraftverkLayer = new VectorLayer({
-  source: new VectorSource({
-    url: "/kws-2100-geographic-information-web-systems/kraftverk.json",
-    format: new GeoJSON(),
-  }),
+  source: clusterSource,
   style: kraftVerkStyle,
 });
 
 function kraftVerkStyle(feature:FeatureLike) {
 
-  const kraftFeature = feature as KraftverkFeature;
+  const kraftFeatures = feature.get('features') as Feature<Point>[];
+
+  const maxMaksytelse = kraftFeatures.reduce((max, feature) => {
+    const maksytelse = (feature.getProperties() as kraftverkProps).maksytelse;
+    return Math.max(max, maksytelse);
+  }, 0);
 
   return new Style({
-    image: new Circle({
-      radius: kraftFeature.getProperties().maksytelse * 10,
+    image: new CircleStyle({
+      radius: maxMaksytelse * 10,
       fill: new Fill({
         color: "red",
       }),
@@ -48,14 +61,14 @@ const KraftverkCheckbox = () => {
   useLayer(checked, kraftverkLayer);
 
   return (
-    <label>
-      {checked ? "Remove kraftverk layer" : "Add kraftverk layer"}
-      <input
-        type="checkbox"
-        checked={checked}
-        onChange={(e) => setChecked(e.target.checked)}
-      />
-    </label>
+      <label>
+        {checked ? "Remove kraftverk layer" : "Add kraftverk layer"}
+        <input
+            type="checkbox"
+            checked={checked}
+            onChange={(e) => setChecked(e.target.checked)}
+        />
+      </label>
   );
 };
 
