3fa23958daeeea1df5c5e7249a6f180f2d0447fa
Simen Didrichsen <108331982+didrichsen@users.noreply.github.com>
2024-03-13 11:06:15 +0100

    backend reading from database

--- server/server.ts
+++ server/server.ts
@@ -8,8 +8,16 @@ const postgresql = new pg.Pool({
 
 const app = express();
 
-app.get("/api/kommuner", (req, res) => {
-  console.log("Lets go!");
+app.get("/api/kommuner", async (req, res) => {
+  const result = await postgresql.query("select kommunenummer,kommunenavn,st_simplify(st_transform(omrade,4326),0.001)::json as geometry from kommuner");
+  res.json({
+    type:"FeatureCollection",
+    features:result.rows.map(({kommunenummer,kommunenavn, geometry}) => ({
+      geometry,
+      type:"Feature",
+      properties:{kommunenummer,kommunenavn}
+    }))
+  });
 });
 
 app.listen(3000);
